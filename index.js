bedf6502ccd93ecdecb0ff6d7dbe896c0101eae2
jean-jacques GONTY <gtyprod@gonty.fr>
2025-12-07 17:26:41 +0100

    Ajout de HTML et test du mode watch

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Bravo ! Votre environnement Node + Git fonctionne !');
+  // On envoie du HTML (Titre h1 + paragraphe)
+  res.send('<h1>Bienvenue sur mon serveur !</h1><p>Ceci est ma première modification. Mais pas la dernière</p>'); 	
 });
 
 app.listen(3000, () => {
