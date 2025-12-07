be8a003d4f7798631ed0fda25bc53d845a86af4d
jean-jacques GONTY <gtyprod@gonty.fr>
2025-12-07 18:32:09 +0100

    Utilisation d'un vrai fichier index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
+const path = require("path"); // 1. On importe l'outil "path" (chemin)
+
 const app = express();
 
-app.get('/', (req, res) => {
-  // On envoie du HTML (Titre h1 + paragraphe)
-  res.send('<h1>Bienvenue sur mon serveur !</h1><p>Ceci est ma première modification. Mais pas la dernière</p>'); 	
+app.get("/", (req, res) => {
+  // 2. Au lieu d'envoyer du texte, on envoie le fichier
+  // __dirname veut dire "le dossier où je suis actuellement"
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(3000, () => {
-  console.log('Serveur lancé sur http://localhost:3000');
+  console.log("Serveur lancé sur http://localhost:3000");
 });
